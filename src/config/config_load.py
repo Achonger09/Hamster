040623cd2722c706b3a56ac0d9686f349b7618ac
chonger09 <987456188@qq.com>
2020-04-25 07:56:39 +0800

    init test

---
+++ src/config/config_load.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import configparser
+import os
+
+class ConfigLoad(object):
+    '''
+    读取config.ini文件内容，并返回
+    '''
+    def __init__(self):
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        print(root_path)
+        config_path = os.path.join(root_path,"..\\..\\config\\config.ini")
+        print(config_path)
+        self.conf= configparser.ConfigParser()
+        self.conf.read(config_path)
+
+    def get(self,*args):
+        return self.conf.get(*args)
+
+'''
+if __name__ == '__main__':
+    config = ConfigLoad()
+    size = config.get("windows","size")
+    print(size)
+'''
